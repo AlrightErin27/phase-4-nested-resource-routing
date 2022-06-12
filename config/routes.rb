5cdf76a4a35e1cb928302bc29f653f0cd09bb3a7
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-11 17:29:12 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
-  resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dog_houses, only: [:show] do
+    resources :reviews, only: %i[show index]
+  end
+
+  resources :reviews, only: %i[show index create]
+
+  # Verb  URI Pattern                                     Controller#Action
+  # dog_house_reviews GET   /dog_houses/:dog_house_id/reviews(.:format)     reviews#index
+  # dog_house_review GET    /dog_houses/:dog_house_id/reviews/:id(.:format) reviews#show
+  # dog_house GET           /dog_houses/:id(.:format)                       dog_houses#show
+  # reviews GET             /reviews(.:format)                              reviews#index
+  # POST                    /reviews(.:format)                              reviews#create
 end
